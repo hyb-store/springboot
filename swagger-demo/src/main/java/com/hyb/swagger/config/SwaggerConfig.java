c58550a68c44bc8fe8a9a6519646ed0155996e71
hyb-store <3063687049@qq.com>
2021-05-24 21:57:05 +0800

    swagger

---
+++ swagger-demo/src/main/java/com/hyb/swagger/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.hyb.swagger.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2 //开启swagger2
+public class SwaggerConfig {
+}
